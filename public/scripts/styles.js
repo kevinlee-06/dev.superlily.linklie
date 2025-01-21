f7e98cd12586ae56b20306c5bb258a39b6e1f0dc
kevinlee-06 <cgshyuankai@proton.me>
2025-01-21 19:01:40 +0800

    Add new Tailwind css theme

--- public/scripts/styles.js
+++ public/scripts/styles.js
@@ -1,3 +1,7 @@
 document.getElementById('theme').addEventListener('change', function() {
-    document.body.className = this.value;
+    if (this.value === 'tailwind') {
+        window.location.href = '/public/new.html';
+    } else {
+        document.body.className = this.value;
+    }
 });
